c8295766833cdb1c0e5090fc1645d65f0b357a50
Ritesh Kaushik <riteshkaushik@Riteshs-MacBook-Air.local>
2024-06-02 18:53:24 +0530

    master

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -4,12 +4,10 @@ import lombok.Data;
 
 @Data
 public class PostDto {
+    private long id;
+    private String title;
+    private String description;
+    private String content;
 
-	private long id;
-	private String title;
-	private String description;
-	private String content;
-	
-	
-	
+
 }
